0f0a800139fa5c87d91b4e1b165f456daa6a4b50
Félix Léveillé <flxleveille@gmail.com>
2019-09-10 11:15:46 -0400

    root fix

---
+++ precache-manifest.636d0c2c5d6e2cd6b5544d61fd2f37bf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a82f6d3feff344e7e8a",
+    "url": "/rustyboy.github.io/a984e77def780b8a7f07.module.wasm"
+  },
+  {
+    "revision": "9d4f35425bde369ee71551e9420d176f",
+    "url": "/rustyboy.github.io/index.html"
+  },
+  {
+    "revision": "e940a0b51d371437d7b6",
+    "url": "/rustyboy.github.io/static/css/3.6a12754a.chunk.css"
+  },
+  {
+    "revision": "fbe5ad277a33e931b2e6",
+    "url": "/rustyboy.github.io/static/css/main.fb65dce4.chunk.css"
+  },
+  {
+    "revision": "6a82f6d3feff344e7e8a",
+    "url": "/rustyboy.github.io/static/js/0.92b2d1b2.chunk.js"
+  },
+  {
+    "revision": "e940a0b51d371437d7b6",
+    "url": "/rustyboy.github.io/static/js/3.371d5923.chunk.js"
+  },
+  {
+    "revision": "ceaa34cedec403665a8b",
+    "url": "/rustyboy.github.io/static/js/4.b4d7ca35.chunk.js"
+  },
+  {
+    "revision": "58144eb4f57e3d4ae8e6",
+    "url": "/rustyboy.github.io/static/js/5.1e15f2fd.chunk.js"
+  },
+  {
+    "revision": "e566c7feeb061635d367",
+    "url": "/rustyboy.github.io/static/js/6.97978c79.chunk.js"
+  },
+  {
+    "revision": "fbe5ad277a33e931b2e6",
+    "url": "/rustyboy.github.io/static/js/main.864b427e.chunk.js"
+  },
+  {
+    "revision": "2d66fcd379532fdf680f",
+    "url": "/rustyboy.github.io/static/js/runtime~main.9dabbd37.js"
+  },
+  {
+    "revision": "c0a53f1952a9201272b9e304ef12367e",
+    "url": "/rustyboy.github.io/static/media/debugger-in.c0a53f19.svg"
+  },
+  {
+    "revision": "84550139fd89087d961fc93a5bc1bdb9",
+    "url": "/rustyboy.github.io/static/media/debugger-over.84550139.svg"
+  },
+  {
+    "revision": "4e2bcff3ea0a674df9934d016463f487",
+    "url": "/rustyboy.github.io/static/media/debugger-play.4e2bcff3.svg"
+  },
+  {
+    "revision": "145c4ea57029e7c9f40fc6399e9f62a3",
+    "url": "/rustyboy.github.io/static/media/gameboy-full-o1.145c4ea5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c8f6a940e072ce7eacc6fcbcfe234f7.js"
+  "/rustyboy.github.io/precache-manifest.636d0c2c5d6e2cd6b5544d61fd2f37bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rustyboy.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
